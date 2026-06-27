7f0b7da8d1bbf6d3921d831420ff96c3fecb288b
babyjhewy <jacobahewitt@icloud.com>
2026-06-27 20:30:10 +1000

    Add offline cache service worker

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='coco-jewel-garden-v21';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.json']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
